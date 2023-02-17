818419d351e215413f4b04ca40aa38a42eddca3d
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-02-17 10:59:12 +0100

    refactoring

--- src/main/java/no/fintlabs/gateway/instance/model/File.java
+++ src/main/java/no/fintlabs/gateway/instance/model/File.java
@@ -3,10 +3,8 @@ package no.fintlabs.gateway.instance.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.http.MediaType;
 
 @Data
